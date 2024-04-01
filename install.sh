bf320f7cfcfa10f39cf58f8bfc67e5463e0d91ee
Dmitry Rasskazov <mitya_myzikant@mail.ru>
2024-04-01 21:24:14 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:dmitry-rasskazov/mouse-mover.git /tmp/mouse-mover
+rm -rf /tmp/mouse-mover/
+
+git clone https://github.com/dmitry-rasskazov/mouse-mover.git /tmp/mouse-mover
 
 gcc /tmp/mouse-mover/mouse-mover.c -o /tmp/mouse-mover/mouse-mover -lX11
 cp /tmp/mouse-mover/mouse-mover /usr/bin/mouse-mover
@@ -23,4 +25,4 @@ echo "" >> /etc/systemd/system/mouse-mover.service
 echo "[Install]" >> /etc/systemd/system/mouse-mover.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/mouse-mover.service
 
-systemctl restart mouse-mover.service
+systemctl daemon-reload
